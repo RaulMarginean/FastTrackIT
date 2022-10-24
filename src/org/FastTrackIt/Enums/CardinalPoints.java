32be71d96705df2166534e6824e12435a7885678
RaulMarginean <raul_marginean_bogdan@yahoo.com>
2022-10-24 21:40:19 +0300

    Second Commit curs 11

--- src/org/FastTrackIt/Enums/CardinalPoints.java
+++ src/org/FastTrackIt/Enums/CardinalPoints.java
@@ -14,6 +14,23 @@ public enum CardinalPoints {
         this.symbol = symbol;
     }
 
+    public String toDirection(){
+        return switch (this){
+            case NORTH -> "UP";
+            case SOUTH -> "DOWN";
+            case WEST -> "LEFT";
+            case EAST -> "RIGHT";
+        };
+    }
+    public static CardinalPoints fromSymbol(String symbol){
+        CardinalPoints[] values = CardinalPoints.values();
+        for(CardinalPoints value: values){
+            if(value.symbol.equals(symbol)){
+                return value;
+            }
+        }
+        return null;
+    }
     public String getSymbol() {
         return symbol;
     }
--- src/org/FastTrackIt/Enums/EnumExercise.java
+++ src/org/FastTrackIt/Enums/EnumExercise.java
@@ -1,10 +1,27 @@
 package org.FastTrackIt.Enums;
 
+import java.util.Scanner;
+
 public class EnumExercise {
     public static void main(String[] args) {
-       CardinalPoints north = CardinalPoints.NORTH;
-       CardinalPoints north2 = CardinalPoints.NORTH;
+        CardinalPoints north = CardinalPoints.NORTH;
+        CardinalPoints north2 = CardinalPoints.NORTH;
 
         System.out.println(north.getSymbol());
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        CardinalPoints fromKeyboard = CardinalPoints.valueOf(s);
+        System.out.println(String.format("Your cardinal point has the symbol %s", fromKeyboard.getSymbol()));
+        switch (fromKeyboard) {
+            case NORTH -> System.out.println("Go Forward, by looking North");
+            case EAST -> System.out.println("Go Right, by looking North");
+            case WEST -> System.out.println("Go Left, by looking North");
+            case SOUTH -> System.out.println("Go in opposite direction, by looking North");
+        }
+        System.out.println("Enter a cardinal symbol: ");
+        String symbolString = scanner.nextLine();
+        CardinalPoints cardinalPoints = CardinalPoints.fromSymbol(symbolString);
+        System.out.println(cardinalPoints.toDirection());
     }
 }
